ec62e107946b208e0bce4f4b7b8bf051cc169a6b
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-07-06 15:36:37 -0400

    completed fetch functionality to target and render image of choice

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all'
 
 const select = document.querySelector('.breeds')
 
-//document.querySelector()
-
 fetch(BREEDS_URL) 
     .then(res => res.json()) // parse response as JSON
     .then(data => {
@@ -14,11 +12,28 @@ fetch(BREEDS_URL)
         for (let i = 0; i < breedsArray.length; i++) {
             const option = document.createElement('option') //<option></option>
             option.value = breedsArray[i] //<option value='{each breed as it iterates through}'>
-            option.innerText = breedsArray[i] //adds the breed text as it iterates through
+            option.innerText = breedsArray[i] //adds the breed text inside option tags as it iterates through
             select.appendChild(option) //append each option to the const select above (which is the <select> dropdown box)
-        }
-        
+        }   
     })
+
+    select.addEventListener('change', event => {
+        console.log(event)
+        console.log(event.target.value)
+        let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`
+        getDoggoImg(url)
+    })
+
+    const img = document.querySelector('.dog-image')
+
+    const getDoggoImg = url => {
+        fetch(url)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {    
+                console.log(data.message)     
+                img.src = data.message      
+            })
+    }
 
 
 //////////////////////////////////////////////////////////////////
@@ -34,8 +49,6 @@ fetch(BREEDS_URL)
 //       .then(res => res.json()) // parse response as JSON
 //       .then(data => {
 //         console.log(data.message)
-        
-
 //       })
 //       .catch(err => {
 //           console.log(`error ${err}`)
